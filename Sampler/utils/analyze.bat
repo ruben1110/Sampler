95e56d3e267682c1f764d9f4f77182e8341c56e2
Ru <alejandrolcjr@gmail.com>
2025-10-29 12:14:15 -0600

    code of this bat file changed for the one generated, which should also work alright

--- Sampler/utils/analyze.bat
+++ Sampler/utils/analyze.bat
@@ -1,9 +1,41 @@
 @echo off
-setlocal enabledelayedexpansion
-set "utils_folder=C:\Users\R\Downloads\ABCD_SERVOMECANISMOS\practica4\Sampler\utils"
+setlocal
+
+:: Define paths (use quotes to handle spaces)
 set "root_folder=C:\Users\R\Downloads\ABCD_SERVOMECANISMOS\practica4\Sampler"
-cd /d %root_folder%
-call .\Sampler_venv\Scripts\activate
-cd /d %utils_folder%
-python analyze.py
-pause
+set "utils_folder=%root_folder%\utils"
+set "venv_activate=%root_folder%\Sampler_venv\Scripts\activate.bat"
+
+:: --- Change to root folder (with drive!) ---
+echo Changing to root folder: %root_folder%
+cd /d "%root_folder%" || (
+    echo ERROR: Could not change to %root_folder%
+    pause
+    exit /b 1
+)
+
+:: --- Activate virtual environment ---
+echo Activating virtual environment...
+call "%venv_activate%" || (
+    echo ERROR: Failed to activate virtual environment at %venv_activate%
+    pause
+    exit /b 1
+)
+
+:: --- Change to utils folder ---
+echo Changing to utils folder: %utils_folder%
+cd /d "%utils_folder%" || (
+    echo ERROR: Could not change to %utils_folder%
+    pause
+    exit /b 1
+)
+
+:: --- Run Python script ---
+echo Running analyze.py...
+python analyze.py || (
+    echo ERROR: Python script failed.
+    pause
+    exit /b 1
+)
+
+exit
