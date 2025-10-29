1d6be8523c0b4d030a500ae0248b6d664a899c7c
Ru <alejandrolcjr@gmail.com>
2025-10-29 12:01:23 -0600

    fixing paths for activating the venv and executing the matlab analysis

--- Sampler/utils/analyze.bat
+++ Sampler/utils/analyze.bat
@@ -1,6 +1,9 @@
 @echo off
-cd ..
-.\Sampler_venv\Scripts\activate
-cd ..\..\utils
+setlocal enabledelayedexpansion
+set "utils_folder=C:\Users\R\Downloads\ABCD_SERVOMECANISMOS\practica4\Sampler\utils"
+set "root_folder=C:\Users\R\Downloads\ABCD_SERVOMECANISMOS\practica4\Sampler"
+cd /d %root_folder%
+call .\Sampler_venv\Scripts\activate
+cd /d %utils_folder%
 python analyze.py
 pause
